c8af0df6b607e84a4cbddc687f93f83f80ef5179
shivkumarbasavraj-hash <shivkumarbasavraj@gmail.com>
2026-02-09 15:39:11 +0100

    Add CarType enum with display names and rates

---
+++ src/main/java/com/rental/model/CarType.java
@@ -0,0 +1,24 @@
+package src.main.java.com.rental.model;
+
+public enum CarType {
+    SEDAN("SEDAN", 50.0),
+    SUV("SUV", 75.0),
+    VAN("Van", 100.0);
+
+private final String displayName;
+private final double dailyRate;
+
+CarType(String displayName, double dailyRate){
+  this.displayName = displayName;
+  this.dailyRate = dailyRate;
+}
+
+public String getDisplayName(){
+  return displayName;
+}
+
+public double getDailyRate(){
+  return dailyRate;
+}
+
+}
